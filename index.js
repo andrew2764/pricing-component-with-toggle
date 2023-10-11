5e2ea1e81be9e7672ef2f877c17c98cead12d8a1
andrew <andrew2764@gmail.com>
2023-10-11 18:20:44 +0800

    Update initial state

--- index.js
+++ index.js
@@ -3,14 +3,20 @@ const basicPrice = document.querySelector("#basic h1");
 const processionalPrice = document.querySelector("#professional h1");
 const masterPrice = document.querySelector("#master h1");
 
+updatePrices(toggle.checked);
+
 toggle.addEventListener("click", (e) => {
-  if (e.target.checked) {
-    basicPrice.innerHTML = "<span>&dollar;</span>19.99";
-    processionalPrice.innerHTML = "<span>&dollar;</span>24.99";
-    masterPrice.innerHTML = "<span>&dollar;</span>39.99";
-  } else {
+  updatePrices(e.target.checked);
+});
+
+function updatePrices(checked) {
+  if (checked) {
     basicPrice.innerHTML = "<span>&dollar;</span>199.99";
     processionalPrice.innerHTML = "<span>&dollar;</span>249.99";
     masterPrice.innerHTML = "<span>&dollar;</span>399.99";
+  } else {
+    basicPrice.innerHTML = "<span>&dollar;</span>19.99";
+    processionalPrice.innerHTML = "<span>&dollar;</span>24.99";
+    masterPrice.innerHTML = "<span>&dollar;</span>39.99";
   }
-});
+}
